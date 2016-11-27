36e76f1d41f08c1110d3a511d8a21008cf4e2082
TomWenman19 <TomWenman19@gmail.com>
2016-11-27 22:14:47 +0000

    Create a particle system and updated backlog

---
+++ NewBuild/Assets/Scripts/Particles.cs
@@ -0,0 +1,20 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Particles : MonoBehaviour {
+
+    void Start()
+    {
+        // You can use particleSystem instead of
+        // gameObject.particleSystem.
+        GetComponent<ParticleSystem>().emissionRate = 0;
+    }
+
+    void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.E))
+        {
+            GetComponent<ParticleSystem>().Emit(10);
+        }
+    }
+}
