222d43c048e1c40ae749fc18399ba9aad8e77c97
TomWenman19 <TomWenman19@gmail.com>
2016-12-05 16:52:01 +0000

    Created the time script and implemented text to appear

---
+++ NewBuild/Assets/Scripts/Timer.cs
@@ -0,0 +1,28 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Timer : MonoBehaviour {
+
+    float TimeRemaining = 60;
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        TimeRemaining -= Time.deltaTime;
+	}
+
+    void OnGUI()
+    {
+        if(TimeRemaining > 0)
+        {
+            GUI.Label(new Rect(700, 100, 200, 100), "Time Remaining : " + (int)TimeRemaining);
+        }
+        else
+        {
+            GUI.Label(new Rect(100, 100, 100, 100), "Time's up");
+        }
+    }
+}
